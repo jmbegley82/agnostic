566b4a72f9e7ad49ebdb6e2c1d71726df01cf966
James Begley <jmbegley@gmx.com>
2021-02-26 13:41:47 -0500

    added support for building under msys/mingw, needs testing!

--- src/Filesystem.cpp
+++ src/Filesystem.cpp
@@ -29,6 +29,9 @@ const char* GetHomeDir() {
 		//_homedir = getenv("HOME");
 		_strHome = string(getenv("HOME")) + "/";
 		_homedir = _strHome.c_str();
+#elif defined (_WIN32) || (_WIN64) || (__CYGWIN__)
+		_strHome = string(getenv("USERPROFILE")) + "/";
+		_homedir = _strHome.c_str();
 #else
 #error Platform support not written yet!
 #endif //platforms
@@ -49,9 +52,14 @@ const char* GetInstallDir() {
 */
 #if defined __SOME_PLATFORM_THAT_TOTALLY_EXISTS
 #else
-#warning Platform not detected; using fallback _installdir of whatever $PWD is during runtime
+#warning Platform not detected; using fallback _installdir of current directory during runtime
+#if defined (__linux__) || defined (__OpenBSD__) || defined (__MACH__)
 		_strInst = string(getenv("PWD")) + "/";
 		_installdir = _strInst.c_str();
+#elif defined (_WIN32) || (_WIN64) || (__CYGWIN__)
+		_strInst = string(getenv("CD")) + "/";
+		_installdir = _strInst.c_str();
+#endif //platforms for fallback
 #endif //platforms
 	}
 	return _installdir;
@@ -62,6 +70,9 @@ const char* GetTmpDir() {
 #if defined (__linux__) || defined (__OpenBSD__) || defined (__MACH__)
 		//_tmpdir = "/tmp";
 		_strTemp = string("/tmp/");
+		_tmpdir = _strTemp.c_str();
+#elif defined (_WIN32) || (_WIN64) || (__CYGWIN__)
+		_strTemp = string(getenv("TEMP"));
 		_tmpdir = _strTemp.c_str();
 #else
 #error Platform support not written yet!
